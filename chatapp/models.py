425da8c21a993854ab5483168699dfa5af12ca22
Sid649 <siddharthsrivastava649@gmail.com>
2023-06-27 13:32:48 +0530

    first commit

---
+++ chatapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# class Contact(models.Model):
+#     user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
+#     friends = models.ManyToManyField
